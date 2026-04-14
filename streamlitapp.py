6aad613444764fa773ec5422735e2442a4ae1212
marulityson <marulityson@gmail.com>
2026-04-14 20:00:32 +0700

    change port to google cloud environment

--- streamlitapp.py
+++ streamlitapp.py
@@ -13,8 +13,8 @@ from requests.exceptions import ChunkedEncodingError, RequestException
 
 load_dotenv()
 
-API_URL = os.getenv("API_URL", "http://localhost:8000/chat/")
-#API_URL = os.getenv("API_URL", "https://olist-agent-181066117930.asia-southeast1.run.app/chat/")
+#API_URL = os.getenv("API_URL", "http://localhost:8000/chat/")
+API_URL = os.getenv("API_URL", "https://olist-agent-181066117930.asia-southeast1.run.app/chat/")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 openai_client = OpenAI(api_key=OPENAI_API_KEY) if OPENAI_API_KEY else None
